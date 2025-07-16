44be02f6349699b6cbc4700b7a1236d719f1a65f
dennissammy77 <dennissammy77@gmail.com>
2025-07-16 19:13:11 +0300

    Created services for db actions

---
+++ backend/server/service.py
@@ -0,0 +1,57 @@
+from config import bcrypt
+from models import db, User, Booking
+import jwt
+
+class UserService():
+    def __init__(self):
+        pass
+    def findById(self,id):
+        if not id == None:
+            return User.query.filter_by(id=id).first()
+        return None
+    
+    def findByEmail(self,email):
+        if not email == None:
+            return User.query.filter_by(email=email).first()
+        return None
+    
+    def findByMobile(self,mobile):
+        if not mobile == None:
+            return User.query.filter_by(mobile=mobile).first()
+        return None
+    
+    def createUser(self, username, mobile, email, password_hash, role):
+        return User(
+            username=username,
+            email=email,
+            mobile=mobile,
+            restaurant_bio="",
+            photo_url="",
+            password_hash=password_hash,
+            role=role
+        )
+    
+    def findAll(self,):
+        return [user.to_dict() for user in User.query.all()]
+    
+class AuthService():
+    def __init__(self):
+        pass
+
+    def hashPassword(self,password):
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    def checkPassword(self,password,user_password):
+        return bcrypt.checkpw(password.encode('utf-8'), user_password)
+    
+    def jwtTokenEncoder(self, body):
+        return jwt.encode(body, 'secret', algorithm="HS256")
+    
+class BookingService():
+    def __init__(self):
+        pass
+
+    def findAll(self):
+        return [booking.to_dict() for booking in Booking.query.all()]
+    
+    
