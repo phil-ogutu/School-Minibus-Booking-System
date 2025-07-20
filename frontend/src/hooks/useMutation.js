8b06614d2be9c35e2f8a39c6a46cce256e68cbd7
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-07-20 17:57:20 +0300

    state data

--- frontend/src/hooks/useMutation.js
+++ frontend/src/hooks/useMutation.js
@@ -1,3 +1,7 @@
+// src/hooks/useMutation.js
 import { useState } from 'react';
 
 export const useMutation = (url, method = 'POST') => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
