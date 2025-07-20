0cc330f87f6e0b13b9c9da7dc48500b802033ea6
dennissammy77 <dennissammy77@gmail.com>
2025-07-20 16:33:01 +0300

    chore: created logo component

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Text from './ui/Text'
+
+function Logo() {
+  return (
+    <Text className='text-dark font-bold text-3xl'>skoolabus</Text>
+  )
+}
+
+export default Logo
