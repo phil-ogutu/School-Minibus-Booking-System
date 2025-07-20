79320f5ff4a586c69ef7c3b97d5fd7e1634a55e9
dennissammy77 <dennissammy77@gmail.com>
2025-07-20 15:51:56 +0300

    feature:driver login page layout responsiveness

--- frontend/src/app/driver/page.jsx
+++ frontend/src/app/driver/page.jsx
@@ -17,16 +17,15 @@ export default function DriverLogin() {
     console.log(values)
   }
   return (
-    <Container className="flex flex-col md:flex-row bg-gradient-gold min-h-screen">
-      
-      <Container className="flex flex-col justify-center items-center w-full md:w-1/2">
-        <Text className="text-primary text-3xl mb-4 font-bold">Driver Login</Text>
+    <Container className="flex flex-col md:flex-row bg-gradient-gold h-screen">
+      <Container className="flex flex-col justify-center items-center w-full md:w-1/2 bg-secondary md:bg-white h-full">
         <FormWrapper
           initialValues={initialValues}
           validationSchema={driverSchema}
           onSubmit={handleSubmit}
           className="bg-white p-6 rounded-lg shadow-md w-full max-w-md"
         >
+          <Text className="text-primary text-center text-3xl mb-4 font-bold">Driver Login</Text>
           <FormField name="name" label="Name" type="text" placeholder="john" />
           <FormField name="password" label="Password" type="password" placeholder="••••••••" />
           <button type="submit" className="bg-primary text-white px-4 py-2 rounded mt-4">
@@ -35,7 +34,7 @@ export default function DriverLogin() {
         </FormWrapper>        
       </Container>
 
-      <Container className="flex flex-col justify-center items-center w-full md:w-1/2 bg-primary">
+      <Container className="hidden md:flex flex-col justify-center items-center w-full md:w-1/2 bg-primary ">
         <Text className="text-dark text-xl">Some carousel goes here</Text>
       </Container>
       
