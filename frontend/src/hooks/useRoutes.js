c64e6354bd2f5020da928642c96c40d02a756734
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-07-20 18:24:55 +0300

    Added createNewRoute method using useMutation

--- frontend/src/hooks/useRoutes.js
+++ frontend/src/hooks/useRoutes.js
@@ -3,10 +3,16 @@ import { useFetch, useMutation } from './useFetch';
 
 export const useRoutes = () => {
   const { data: routes, loading: routesLoading, error: routesError } = useFetch('/api/routes');
+  const { mutate: createRoute } = useMutation('/api/routes');
+
+  const createNewRoute = async (routeData) => {
+    return await createRoute(routeData);
+  };
 
   return {
     routes,
     routesLoading,
     routesError,
+    createNewRoute,
   };
 };
