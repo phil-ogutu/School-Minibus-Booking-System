f1dcf9ab2c893dd9179e79290ddaaf739bc96d1c
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-07-20 17:49:49 +0300

    Initialize state variables for data, loading, and error

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,3 +1,6 @@
 import { useState, useEffect } from 'react';
 
 export const useFetch = (url, options = {}) => {
+    const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
