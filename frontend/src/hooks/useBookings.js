8ffefb314356a6f131525ec6f1920921a1f0541c
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-07-20 18:14:00 +0300

    Added create booking mutation

--- frontend/src/hooks/useBookings.js
+++ frontend/src/hooks/useBookings.js
@@ -2,3 +2,5 @@ import { useFetch, useMutation } from './useFetch';
 
 export const useBookings = () => {
   const { data: bookings, loading: bookingsLoading, error: bookingsError } = useFetch('/api/bookings');
+    const { mutate: createBooking, loading: creatingBooking } = useMutation('/api/bookings');
+
