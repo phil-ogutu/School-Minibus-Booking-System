cd25786dfc8b8fc268d08db56cff1f2782244406
phil-ogutu <phil.shifwoko@gmail.com>
2025-07-21 22:52:53 +0300

    populate routes on booking page with backend

--- frontend/src/app/bookings/page.js
+++ frontend/src/app/bookings/page.js
@@ -4,55 +4,50 @@ import dynamic from "next/dynamic";
 import TopSearchCard from "../../components/SearchCard";
 import SearchBus from "../../components/SearchBus";
 import Navbar from "@/components/Navbar";
-import MapComponent from "@/components/MapComponent";
-import { routesData } from "@/data/RoutesData";
-import { useEffect, useState } from "react";
+// import MapComponent from "@/components/MapComponent";
 import { useTheContext } from "@/context/MapContext";
+import { useRoutes } from "@/hooks/useRoutes";
 
-const Map = dynamic(() => import("@/components/Map"), { ssr: false });
+const MapComponent = dynamic(() => import("@/components/MapComponent"), { ssr: false });
 
 export default function Booking() {
-  const [currentRoutes, setCurrentRoutes] = useState(routesData)
-  const { from, to } = useTheContext(); 
-  console.log("FROM", from)
-  console.log("TO", to)
+  const { routes, routesLoading, routesError } = useRoutes();
+  const { from, to } = useTheContext();
 
-  console.log(Array.isArray(currentRoutes)) // true if it's an array
+  // console.log("FROM", from);
+  // console.log("TO", to);
 
-  const filteredRoutes = currentRoutes.filter((route) => {
-    // return route.id > 0
-    // return route.start.toLowerCase().includes(from.toLowerCase())
-    // return route.end.toLowerCase().includes(to.toLowerCase())
-    const matchFrom = from ? route.start.toLowerCase().includes(from.toLowerCase()) : true;
-    const matchTo = to ? route.end.toLowerCase().includes(to.toLowerCase()) : true;
+  const filteredRoutes = routes?.filter((route) => {
+    const matchFrom = from
+      ? route.start.toLowerCase().includes(from.toLowerCase())
+      : true;
+    const matchTo = to
+      ? route.end.toLowerCase().includes(to.toLowerCase())
+      : true;
 
     return matchFrom && matchTo;
+  });
 
-  })
-  console.log("Filtered routes", filteredRoutes)
-
-  // useEffect(() => {
-  //   setCurrentRoutes(routesData)
-  // }, [])
+  const displayRoutes =
+    filteredRoutes?.length > 0 ? filteredRoutes : routes || [];
+  console.log("Filtered routes", filteredRoutes);
 
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+
       <div className="flex-1 grid grid-cols-2">
         <div className="p-5 flex flex-col space-y-5">
           <SearchBus />
+
           <div className="overflow-y-auto space-y-4 no-scrollbar h-[calc(100vh-220px)]">
 
-            {(filteredRoutes && filteredRoutes.length>0 ? filteredRoutes:currentRoutes).map((route) => (
+            {displayRoutes.map((route) => (
               <div key={route.id}>
                 <TopSearchCard route={route} />
               </div>
             ))}
 
-            {/* <TopSearchCard /> */}
-            {/* <TopSearchCard />
-            <TopSearchCard />
-            <TopSearchCard /> */}
           </div>
         </div>
 
@@ -60,8 +55,6 @@ export default function Booking() {
           <MapComponent />
         </div>
       </div>
-
-      {/* <SearchBus /> */}
     </div>
   );
 }
