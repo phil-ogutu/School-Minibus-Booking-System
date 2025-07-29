1dbd677188dd7e9774dd4e3258c9b36eafd80cab
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-07-29 19:13:22 +0300

    refactored payment page to use correct App-Router pattern

--- frontend/src/app/payment/page.jsx
+++ frontend/src/app/payment/page.jsx
@@ -2,8 +2,16 @@
 "use client";
 import PaymentForm from "@/components/PaymentForm";
 
-export default function PaymentPage({ searchParams }) {
-  const tripId = searchParams?.tripId || "";
+export default async function PaymentPage({ searchParams }) {
+  const tripId = await searchParams; //await here
+
+  if (!tripId) {
+    return (
+      <div className="p-6 text-red-600">
+        Missing tripId. Please go back and select a trip.
+      </div>
+    );
+  }
   return (
     <div className="p-6">
       <h2 className="text-xl font-semibold mb-4">Confirm and Pay</h2>
