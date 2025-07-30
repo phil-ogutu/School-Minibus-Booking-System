b3e4af8d00ebefc471c2cbfd2e94e00d8b5df924
dennissammy77 <dennissammy77@gmail.com>
2025-07-30 08:05:00 +0300

    page access

--- frontend/src/app/parentDashboard/layout.js
+++ frontend/src/app/parentDashboard/layout.js
@@ -1,14 +1,14 @@
-'use client'
-import DashboardHeader from "@/components/DashboardHeader";
-import DashboardSidebar from "@/components/DashboardSidebar";
+"use client";
 import { useAuthContext } from "@/context/AuthContext";
+import ParentDashboardSidebar from "@/components/ParentDashboardSidebar";
+import DashboardHeader from "@/components/DashboardHeader";
 import { useRouter } from "next/navigation";
 
-export default function AdminLayout({ children }) {
+export default function ParentLayout({ children }) {
     const { user, loading, error } = useAuthContext();
     const router = useRouter();
     console.log(user)
-    if(user && user?.role != 'admin'){
+    if(user && user?.role != 'parent'){
         alert('you are not authorized to access this panel');
         router.push('/');
         return;
@@ -16,10 +16,10 @@ export default function AdminLayout({ children }) {
     return (
         <div className="xl:flex h-screen">
             {/* Sidebar */}
-            <DashboardSidebar />
+            <ParentDashboardSidebar />
             {/* Main Content Area */}
             <div className={`flex-1 transition-all duration-300 ease-in-out`} >
-                <DashboardHeader title="Admin Dashboard" />
+                <DashboardHeader title="Parent Dashboard" />
                 <div className="bg-base h-screen overflow-scroll">{children}</div>
             </div>
         </div>
