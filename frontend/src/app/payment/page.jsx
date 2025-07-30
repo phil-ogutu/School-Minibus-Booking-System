9b026de1351f0e43ed34612bc09ed822165aa49c
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-07-30 04:50:43 +0300

    refactored payment page

--- frontend/src/app/payment/page.jsx
+++ frontend/src/app/payment/page.jsx
@@ -1,54 +1,4 @@
-// // This is the page handling payments
-
-// "use client";
-// import PaymentForm from "@/components/PaymentForm";
-// import Navbar from "@/components/Navbar";
-// import Image from "next/image";
-
-// export default async function PaymentPage({ searchParams }) {
-//   const tripId = await searchParams;
-
-//   if (!tripId) {
-//     return (
-//       <div className="p-6 text-red-600">
-//         Missing tripId. Please go back and select a trip.
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="relative min-h-screen bg-gray-100">
-//       {/* Background Bus Image */}
-//       <div className="absolute top-0 left-0 w-full h-[40vh] md:h-[60vh] z-0">
-//         <Image
-//           src="public/bus-hero.png" // Make sure this image exists in /public/images
-//           alt="Bus Hero"
-//           layout="fill"
-//           objectFit="cover"
-//           className="opacity-50"
-//           priority
-//         />
-//       </div>
-
-//       {/* Content */}
-//       <div className="relative z-10">
-//         <Navbar />
-
-//         <div className="max-w-3xl mx-auto mt-[25vh] md:mt-[35vh] p-6 bg-white rounded-lg shadow-lg">
-//           <h2 className="text-2xl font-semibold mb-4 text-center">
-//             Confirm and Pay
-//           </h2>
-//           <p className="text-gray-600 text-sm mb-6 text-center">
-//             You're almost done! Please confirm your booking and complete your
-//             payment below.
-//           </p>
-//           <PaymentForm tripId={tripId} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
+// This page handles payment
 "use client";
 
 // Collect user input for M-pesa STK request
@@ -95,13 +45,13 @@ export default function PaymentForm({ tripId }) {
   };
 
   return (
-    <div className="relative max-w-md w-full bg-white rounded-xl shadow-lg p-6 mt-6 mx-auto">
+    <div className="relative max-w-2xl w-full bg-white rounded-xl shadow-lg p-5 mt-20 mb-20 mx-auto">
       {/* Bus Logo */}
       <div className="absolute top-4 right-4">
-        <Image src="/bus-hero.png" alt="Bus Logo" width={40} height={40} />
+        <Image src="/bus-hero.png" alt="Bus Logo" width={120} height={50} />
       </div>
 
-      <h3 className="text-2xl font-bold text-center mb-4">Mpesa Payment</h3>
+      <h3 className="text-3xl font-bold text-center mb-4">Mpesa Payment</h3>
 
       <form
         onSubmit={(e) => {
