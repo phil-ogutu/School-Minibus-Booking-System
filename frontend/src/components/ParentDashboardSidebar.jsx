531f1a3e02ad05011182ea36e653db00e1091a81
dennissammy77 <dennissammy77@gmail.com>
2025-07-30 08:04:51 +0300

    ui touch up

--- frontend/src/components/ParentDashboardSidebar.jsx
+++ frontend/src/components/ParentDashboardSidebar.jsx
@@ -11,18 +11,17 @@ import {
 } from "react-icons/fa";
 
 const navItems = [
-  { href: "/parentDashboard", label: "Dashboard", icon: FaHome },
-  { href: "/parentDashboard/bookings", label: "View Bookings", icon: FaClipboardList },
-  { href: "/parentDashboard/profile", label: "Edit Profile", icon: FaUserFriends },
+  { href: "/parentDashboard/home", label: "Dashboard", icon: FaHome },
+  { href: "/parentDashboard/bookings", label: "Bookings", icon: FaClipboardList },
+  { href: "/parentDashboard/profile", label: "Profile", icon: FaUserFriends },
 ];
 
 export default function DashboardSidebar() {
   const pathname = usePathname();
 
   return (
-    <aside className="w-64 h-screen bg-[#0F333F] text-white flex flex-col p-6">
-      <h2 className="text-3xl font-bold mb-8">SkoolaBus</h2>
-
+    <aside className="w-64 h-screen bg-base text-dark flex flex-col p-6 border-r border-slate-200">
+      <h2 className="text-3xl font-bold mb-8 text-primary">SkoolaBus</h2>
       <nav className="flex flex-col space-y-2">
         {navItems.map(({ href, label, icon: Icon }) => {
           const isActive = pathname.startsWith(href);
@@ -30,11 +29,11 @@ export default function DashboardSidebar() {
             <Link
               key={href}
               href={href}
-              className={`flex items-center space-x-2 px-3 py-2 rounded-md transition-colors duration-200
+              className={`flex items-center space-x-2 px-3 py-2 my-2 rounded-md transition-colors duration-200 text-xl
                 ${
                   isActive
-                    ? "bg-[#0A252F] text-amber-400"
-                    : "hover:bg-[#0A252F] hover:text-amber-400"
+                    ? "bg-primary text-white fw-bold"
+                    : "hover:bg-tertiary hover:text-dark"
                 }`}
             >
               <Icon /> <span>{label}</span>
